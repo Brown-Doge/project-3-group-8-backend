e654efbf2f9cfc3fcbb3056952138d9054e1f4fa
Jorge <jbarrera-simental@csumb.edu>
2025-10-28 17:25:36 -0700

    Implemented OAuth with Spring Security (using environment variables).

--- src/main/java/com/example/EventLink/controller/HomeController.java
+++ src/main/java/com/example/EventLink/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "Hello, home!";
     }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "Home, Secured!";
+    }
 }
